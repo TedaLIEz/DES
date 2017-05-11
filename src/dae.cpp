fe4e6019877822b4a08ae80dad9db84305d6c571
TedaLIEz <aliezted@gmail.com>
2017-05-11 11:07:04 +0800

    feat: f func

--- src/dae.cpp
+++ src/dae.cpp
@@ -4,7 +4,16 @@
 #include "dae.h"
 
 void DAE::setBit(uint64_t &input, int index, bool zero) {
-  uint64_t mask = (uint64_t) (1) << 63 - (index);
+  uint64_t mask = (uint64_t) (1) << (63 - index);
+  if (!zero) {
+    input = input | mask;
+  } else {
+    input = input & (~mask);
+  }
+}
+
+void DAE::setBit(uint32_t &input, int index, bool zero) {
+  uint32_t mask = (uint32_t) (1) << (31 - index);
   if (!zero) {
     input = input | mask;
   } else {
@@ -13,13 +22,13 @@ void DAE::setBit(uint64_t &input, int index, bool zero) {
 }
 
 int DAE::getBit(uint64_t input, int index) {
-  input = input >> 63 - (index);
+  input = input >> (63 - index);
 
   return (int) (input & 1);
 }
 
 int DAE::getBit(uint32_t input, int index) {
-  input = input >> 31 - (index);
+  input = input >> (31 - index);
   return (int) (input & 1);
 }
 uint32_t DAE::leftPart(uint64_t in) {
@@ -40,11 +49,9 @@ uint64_t DAE::toIP(uint64_t in) {
   return rst;
 }
 
-
-uint32_t DAE::f(uint32_t in) {
-  return 0;
+uint32_t DAE::f(uint32_t in, bitset<48> k) {
+  return p(s(e(in) ^ k));
 }
-
 
 bitset<48> DAE::e(uint32_t in) {
   bitset<48> rst;
@@ -55,11 +62,32 @@ bitset<48> DAE::e(uint32_t in) {
   return rst;
 }
 
+// Done!
 uint32_t DAE::s(bitset<48> in) {
-
+  uint32_t rst = 0;
+  for (int i = 0; i < 8; i++) {
+    int s = (int) subbitset(in, 48 - (i + 1) * 6, 48 - i * 6).to_ulong();
+    int tmp = sbox(i, s);
+    rst |= (tmp << (28 - (i) * 4));
+  }
+  return rst;
 }
 
-DAE::DAE() {
+int DAE::sbox(int i, int s) {
+  return smap[i][s];
+}
+
+uint32_t DAE::p(uint32_t in) {
+  uint32_t rst = in;
+  for (int i = 0; i < 32; ++i) {
+    setBit(rst, i, getBit(in, pmap[i] - 1) == 0);
+  }
+  return rst;
+}
+
+
+
+DAE::DAE(uint64_t key) {
   smap.insert({0, s1map});
   smap.insert({1, s2map});
   smap.insert({2, s3map});
