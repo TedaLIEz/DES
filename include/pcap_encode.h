846a0b15c6e3a156ba48a8c651b5715344809786
TedaLIEz <aliezted@gmail.com>
2017-05-31 22:32:25 +0800

    feat: pcap header struct

---
+++ include/pcap_encode.h
@@ -0,0 +1,34 @@
+//
+// Created by aLIEzTed on 5/31/17.
+//
+
+#ifndef DES_PCAP_ENCODE_H
+#define DES_PCAP_ENCODE_H
+
+typedef struct pcap_hdr_s {
+  uint32_t magic_number;   /* magic number */
+  uint16_t version_major;  /* major version number */
+  uint16_t version_minor;  /* minor version number */
+  int32_t  thiszone;       /* GMT to local correction */
+  uint32_t sigfigs;        /* accuracy of timestamps */
+  uint32_t snaplen;        /* max length of captured packets, in octets */
+  uint32_t network;        /* data link type */
+} pcap_hdr_t;
+
+typedef struct pcaprec_hdr_s {
+  uint32_t ts_sec;         /* timestamp seconds */
+  uint32_t ts_usec;        /* timestamp microseconds */
+  uint32_t incl_len;       /* number of octets of packet saved in file */
+  uint32_t orig_len;       /* actual length of packet */
+} pcaprec_hdr_t;
+
+typedef struct pcaprec_s {
+  pcaprec_hdr_t hdr;         /* packet header */
+  void* data;                /* packet data */
+} pcaprec_t;
+
+class PcapEncoder {
+
+};
+
+#endif //DES_PCAP_ENCODE_H
